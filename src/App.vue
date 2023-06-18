be28737e0827a884e76c9746fb1eb29f60916624
Ryan <56096817+Youxiaaa@users.noreply.github.com>
2023-06-19 00:30:58 +0800

    [UPDATE] Vue3.3 use defineModel

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Chile from './components/Chile.vue'
+import { ref } from 'vue'
+
+const title = ref<string>('Vue 3.3 defineModel')
+const message = ref<string>('')
+</script>
+
+<template>
+  <Chile v-model:title="title" v-model:input="message" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Chile.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const titleModel = defineModel<string>('title', {
+  required: true
+})
+const inputModel = defineModel<string>('input', {
+  required: true
+})
+</script>
+
+<template>
+  <h1>{{ titleModel }}</h1>
+  <input v-model="inputModel" type="text">
+</template>
